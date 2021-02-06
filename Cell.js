55ce3eef7e9854f469a7d00d11de93978316bde7
Bridger Jones <bridgerjones15@gmail.com>
2021-02-05 21:19:39 -0700

    creating initial data structure for a cell

---
+++ Cell.js
@@ -0,0 +1,99 @@
+class Cell {
+  left = {
+    x: null,
+    y: null
+  }
+  top = {
+    x: null,
+    y: null
+  }
+  right = {
+    x: null,
+    y: null
+  }
+  bottom = {
+    x: null,
+    y: null
+  }
+
+  isWall = true;
+
+  constructor(x, y, xMax, yMax){
+    this.x = x;
+    this.y = y;
+    // top left corner
+    if (x == 0 && y == 0) {
+      this.right.x = x + 1;
+      this.right.y = y;
+      this.bottom.x = x;
+      this.bottom.y = y + 1;
+    }
+    // top right corner
+    else if (x == xMax && y == 0){
+      this.left.x = x - 1;
+      this.left.y = y;
+      this.bottom.x = x;
+      this.bottom.y = y + 1;
+    }
+    // bottom right corner
+    else if (x == xMax && y == yMax){
+      this.top.x = x;
+      this.top.y = y - 1;
+      this.left.x = x - 1;
+      this.left.y = y;
+    }
+    // bottom left corner
+    else if (x == 0 && y == yMax){
+      this.top.x = x;
+      this.top.y = y - 1;
+      this.right.x = x + 1;
+      this.right.y = y;
+    }
+    // left edge
+    else if (x == 0){
+      this.top.x = x;
+      this.top.y = y - 1;
+      this.right.x = x + 1;
+      this.right.y = y;
+      this.bottom.x = x;
+      this.bottom.y = y + 1;
+    }
+    // top edge
+    else if (y == 0){
+      this.left.x = x - 1;
+      this.left.y = y;
+      this.right.x = x + 1;
+      this.right.y = y;
+      this.bottom.x = x;
+      this.bottom.y = y + 1;
+    }
+    // right edge
+    else if (x == xMax){
+      this.left.x = x - 1;
+      this.left.y = y;
+      this.top.x = x;
+      this.top.y = y - 1;
+      this.bottom.x = x;
+      this.bottom.y = y + 1;
+    }
+    //bottom edge
+    else if (y == yMax){
+      this.left.x = x - 1;
+      this.left.y = y;
+      this.top.x = x;
+      this.top.y = y - 1;
+      this.right.x = x + 1;
+      this.right.y = y;
+    }
+    else {
+      this.left.x = x - 1;
+      this.left.y = y;
+      this.top.x = x;
+      this.top.y = y - 1;
+      this.right.x = x + 1;
+      this.right.y = y;
+      this.bottom.x = x;
+      this.bottom.y = y + 1;
+    }
+  }
+}
